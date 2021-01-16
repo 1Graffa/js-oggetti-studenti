ab41bccfb57279823f9dc35b5a5804300b5d1636
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-16 12:12:46 +0100

    implemento maiuscole-minuscole

--- js/script.js
+++ js/script.js
@@ -36,10 +36,33 @@ for (var i = 0; i < superEroi.length; i++) {
       }
   }
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-var nomeUtente = prompt("Insert your name");
-var potereUtente = prompt("Qual è il tuo superpotere?")
-var gruppoUtente = prompt("A che gruppo appartieni?")
+var nomeUtente = prompt("Inserisci il nome di un supereroe");
+var potereUtente = prompt("Qual è il suo superpotere?");
+var gruppoUtente = prompt("A che gruppo appartiene?");
+
+// funzione per regolare maiuscole e minuscole
+// maiusMinus(nomeUtente);
+// maiusMinus(potereUtente);
+// maiusMinus(gruppoUtente);
+
+//correggo maiuscole e minuscole (senza funzione)
+toUp = nomeUtente.charAt(0).toUpperCase();
+toLow = nomeUtente.substring(1).toLowerCase();
+nomeUtente = toUp + toLow;
+toUp = potereUtente.charAt(0).toUpperCase();
+toLow = potereUtente.substring(1).toLowerCase();
+potereUtente = toUp + toLow;
+toUp = gruppoUtente.charAt(0).toUpperCase();
+toLow = gruppoUtente.substring(1).toLowerCase();
+gruppoUtente = toUp + toLow;
 
 superEroi.push({name : nomeUtente , power : potereUtente , group : gruppoUtente});
 
 console.log(superEroi);
+
+//Funzione per regolare automaticamente maiuscole e minuscole di una stringa
+function maiusMinus(stringa){
+  toUp = stringa.charAt(0).toUpperCase();
+  toLow = stringa.substring(1).toLowerCase();
+  stringa = toUp + toLow;
+}

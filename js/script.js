b85d8bdd98af9054c265c5987a910f31624f7f74
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-18 15:21:20 +0100

    ripasso

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
     name : "Ciclope",
     power : "Optic Blasts",
     group : "X-Men"
-  }
+  };
 //stampo a schermo attraverso il for in tutte le proprietà
   for (var k in superEroe) {
     document.getElementById("nome").innerHTML = "Name : " + superEroe.name;
@@ -31,38 +31,58 @@ var superEroi =  [
   }
 ];
 for (var i = 0; i < superEroi.length; i++) {
-    for (var k in superEroi[i]) {
+    for (var k in superEroi[i]) {//ATTENZIONE:meglio usare ciclo for per array
       document.getElementById("prova").innerHTML = superEroi[0].name + " of " + superEroi[0].group + " , " + superEroi[1].name + " of " + superEroi[1].group + " , " + superEroi[2].name + " of " + superEroi[2].group;
       }
   }
+//  Ecco con il cliclo for
+//for (var i = 0; i < superEroi.length; i++)
+//console.log(superEroi[i].name," ",superEroi[i].group)
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+
 var nomeUtente = prompt("Inserisci il nome di un supereroe");
 var potereUtente = prompt("Qual è il suo superpotere?");
 var gruppoUtente = prompt("A che gruppo appartiene?");
 
 // funzione per regolare maiuscole e minuscole
-// maiusMinus(nomeUtente);
-// maiusMinus(potereUtente);
-// maiusMinus(gruppoUtente);
+nomeUtente = maiusMinus(nomeUtente);
+potereUtente = maiusMinus(potereUtente);
+gruppoUtente = maiusMinus(gruppoUtente);
 
-//correggo maiuscole e minuscole (senza funzione)
-toUp = nomeUtente.charAt(0).toUpperCase();
-toLow = nomeUtente.substring(1).toLowerCase();
-nomeUtente = toUp + toLow;
-toUp = potereUtente.charAt(0).toUpperCase();
-toLow = potereUtente.substring(1).toLowerCase();
-potereUtente = toUp + toLow;
-toUp = gruppoUtente.charAt(0).toUpperCase();
-toLow = gruppoUtente.substring(1).toLowerCase();
-gruppoUtente = toUp + toLow;
+//sarebbe stato meglio creare una variabile per il nuovo superEroe COSI':
+//var nuovoInput ={
+//   name : nomeUtente,
+//   power : potereUtente,
+//   group : gruppoUtente
+// };
+// superEroi.posh(nuovoInput)
 
+// correggo maiuscole e minuscole (senza funzione)
+// toUp = nomeUtente.charAt(0).toUpperCase();
+// toLow = nomeUtente.substring(1).toLowerCase();
+// nomeUtente = toUp + toLow;
+// toUp = potereUtente.charAt(0).toUpperCase();
+// toLow = potereUtente.substring(1).toLowerCase();
+// potereUtente = toUp + toLow;
+// toUp = gruppoUtente.charAt(0).toUpperCase();
+// toLow = gruppoUtente.substring(1).toLowerCase();
+// gruppoUtente = toUp + toLow;
+
+// pusho in array i dati presi con il prompt e corretti da maiuscole e minuscole
 superEroi.push({name : nomeUtente , power : potereUtente , group : gruppoUtente});
-
 console.log(superEroi);
 
+
 //Funzione per regolare automaticamente maiuscole e minuscole di una stringa
-function maiusMinus(stringa){
-  toUp = stringa.charAt(0).toUpperCase();
-  toLow = stringa.substring(1).toLowerCase();
-  stringa = toUp + toLow;
+
+function maiusMinus(parola) {
+    return parola.charAt(0).toUpperCase() + parola.substring(1).toLowerCase();
 }
+// nb, così non da:
+// function maiusMinus(stringa){
+//   var toUp = stringa.charAt(0).toUpperCase();
+//   var toLow = stringa.substring(1).toLowerCase();
+//   var stringa = toUp + toLow;
+//
+// }
